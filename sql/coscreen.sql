1f46a6ddffc9c1636b375ec7a094bfdb35bf37fb
Thy <thierry@ovh1>
2016-06-23 17:14:47 +0200

    coscreen for 2013 & 2014.

--- sql/coscreen.sql
+++ sql/coscreen.sql
@@ -1,3 +1,4 @@
+/*
 drop index if exists i_coscreen_2015;
 drop table if exists coscreen_2015;
 
@@ -21,4 +22,43 @@ create table coscreenranked_2015 as (
        select ka, kb, n,
        	      rank() over (partition by ka order by n desc) as rnk
        from coscreenag_2015);
+*/
+
+--
+-- computes coscreening for a given year.
+-- example : select create_coscreen ('2014')
+--
+
+create or replace function create_coscreen (y text) returns void as $$
+begin
+	execute format ('drop index if exists i_coscreen_%s', y);
+	execute format ('drop table if exists coscreen_%s', y);
+	execute format ('
+		create table coscreen_%s as (
+       		       select A.screeningid as ka,
+       	      	       	      B.screeningid as kb
+      			from reelport_%s as A, reelport_%s as B
+       			where A.screensec > 0 and B.screensec > 0 and A.buyerid = B.buyerid)',
+		y,y,y);
+	execute format ('create index i_coscreen_%s on coscreen_%s(ka, kb)', y, y);
+	
+	execute format ('drop index if exists i_coscreenag_%s_kb', y);
+	execute format ('drop table if exists coscreenag_%s', y);
+	execute format ('
+		create table coscreenag_%s as (
+       		       select ka, kb, count(*) as n
+       		       from coscreen_%s group by ka, kb)',
+		y, y);
+	execute format ('create index i_coscreenag_%s_kb on coscreenag_%s (kb)', y, y);
+
+	execute format ('drop table if exists coscreenranked_%s', y);
+	execute format ('
+		create table coscreenranked_%s as (
+       		       select ka, kb, n,
+       	      	       	      rank() over (partition by ka order by n desc) as rnk
+       			from coscreenag_%s)',
+		y, y);
+	return;
+end
+$$ language plpgsql;
 
